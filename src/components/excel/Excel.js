8bd8462137f033858cc4a3d003613dd5727209ab
Dmitrii <dmi3iva@gmail.com>
2022-02-21 18:33:59 +0300

    M3L5 added some methods for dom class

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,7 +2,7 @@ import {$} from '@core/dom';
 
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector);
+    this.$el = $(selector);
     this.components = options.components || [];
   }
 
@@ -12,7 +12,7 @@ export class Excel {
     this.components.forEach((Component)=>{
       const $el = $.create('div', Component.className);
       const c = new Component($el);
-      $el.innerHTML = c.toHtml();
+      $el.html(c.toHtml());
       $root.append($el);
     });
 
